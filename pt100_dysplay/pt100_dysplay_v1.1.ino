c94f2eeeeafee6cbc8ddead21548ae49856ba40a
Mats Karlsson <mats@tinnio.net>
2014-05-29 09:43:00 +0200

    Translation to English

--- pt100_dysplay/pt100_dysplay_v1.1.ino
+++ pt100_dysplay/pt100_dysplay_v1.1.ino
@@ -1,14 +1,15 @@
 #include <LiquidCrystal.h>
 #include <PID_v1.h>
 
-//Control beta lazo de temperatura con rtd pt100
-//Autor: Sergio Pablo Peñalve
-//catriel - Rio Negro
+// Control loop beta temperature with PT100 RTD
+// Autor: Sergio Pablo Peñalve
+// catriel - Rio Negro
+// Translation to English: Mats Karlsson [2014]
 
-//LiquidCrystal lcd(8, 9, 4, 5, 6, 7); //version convencional
+// LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  // Conventional version
 
-//moificacion 
-LiquidCrystal lcd(6, 7, 2, 3, 4, 5); //usamos la primer filas de pines
+// Modification
+LiquidCrystal lcd(6, 7, 2, 3, 4, 5);     // Use the first row of pins
 
 double Setpoint, Input, Output;
 PID myPID(&Input, &Output, &Setpoint,5,0.2,0.5, DIRECT);
@@ -16,44 +17,45 @@ PID myPID(&Input, &Output, &Setpoint,5,0.2,0.5, DIRECT);
 //#define _uno
  #define _mega_2560
  
-//metodo de calibracion 
+// Calibration method
 
-//Usar un baño termostatico seco o de aceite a falta del mismo usar termometro,pirometro o termocupla
-//agua congelada y agua hervida
+// Use a dry or oil bath thermostatic in lack thereof use thermometer, pyrometer or
+// thermocouple frozen water and boiled water
 
-//La RTD PT100 varia la resistencia de manera lineal a la temperatura por lo que tomando dos puntos en baja temp y alta temp
-//podemos generar una funcion lineal con estos puntos y transformar el valor ADC en grados C°
+// The PT100 RTD resistance varies linearly with the temperature so meassuring two points
+// in low temp and high temp can generate a linear function with these points and
+// transform the ADC value in degrees C°.
 
 
-//otra forma es calculando la resistencia el voltaje y por regla de tres con respecto al valor ADC obtener la T°
+// Another way is to calculate the voltage and resistance rule of three to the value ADC obtain T°
 
  
-float Ay1= 17.2;  //1 RTD °C puntos de calibracion frio
-float Ax1=306; //adc
+float Ay1= 17.2;  // 1st RTD °C cold calibration points
+float Ax1=306;    // ADC
 
-float By1= 17.2;  //2 RTD °C puntos de calibracion frio
-float Bx1=302; //adc
+float By1= 17.2;  // 2nd RTD °C cold calibration points
+float Bx1=302;    // ADC
 
-float Ay2=60; //1 RTD °C puntos de calibracion caliente
-float Ax2=623; //adc
+float Ay2=60;     // 1st RTD °C hot calibration points
+float Ax2=623;    // ADC
  
-float By2=60; ///1 RTD °C puntos de calibracion caliente
-float Bx2=618; //adc
+float By2=60;     // 2nd RTD °C hot calibration points
+float Bx2=618;    // ADC
 
 int t1,t2,i,cant;
 float acum1,acum2,m1,m2,c1,c2,temp1,temp2;
-int setP=15; //temperatura estandar para caudal u otros
+int setP=15;      // Standard temperature for flow or other
 
 void setup()
 {
   cant=10000;
- 
+
   #ifdef _uno    
-  analogReference(INTERNAL); //arduino uno
+  analogReference(INTERNAL);     // Arduino UNO
   #endif
        
   #ifdef _mega_2560    
-  analogReference(INTERNAL1V1); //arduino mega 2560
+  analogReference(INTERNAL1V1);  // Arduino Mega 2560
   #endif
   
   Serial.begin(9600);
@@ -61,26 +63,24 @@ void setup()
   myPID.SetMode(AUTOMATIC);
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
-  
-  
 }
 
+
 void loop()
-{ 
-    
-  //muestreo de señal analogica
+{
+  // Sampling analog signal
   for (i=0;i<10000;i++)
   {
   t1 = analogRead(1);
   t2 = analogRead(2);
-  
+
   acum1=acum1+(float)t1;
   acum2=acum2+(float)t2;
   }
-  
+
   acum1=acum1/cant;
   acum2=acum2/cant;
-  
+
   /*
   if ((acum1 = 0 )||( acum2=0))
   {
@@ -88,44 +88,40 @@ void loop()
   }
   */
  
-  //calculo de temperatura por funcion lineal
+  // Calculate linear function of temperature
   // y=m * b + c
-  // donde m es la pendiente
-  // y - b son los puntos
-  //c es la constante
+  // where m is the slope
+  // y - b are points
+  // c is the constant
    
   m1=pendiente(Ay1,Ax1,Ay2,Ax2);
   m2=pendiente(By1,Bx1,By2,Bx2);
-  
+
   c1=constante(m1,Ay1,Ax1);
   c2=constante(m2,By1,Bx1);
-  
-  
+
   temp1=obt_temp(m1,c1,acum1);
   temp2=obt_temp(m2,c1,acum2);
   lcd.clear();
   lcd.print("PV:"); 
   lcd.print(temp1);
-  
-  
+
   Setpoint=obt_adc(m1,c1,setP);
   lcd.setCursor(0, 1); 
   lcd.print("SP:");
   lcd.print(obt_temp(m1,c1,Setpoint));
-  
- // chequeo de falla de alguna RTD
+
+ // Check fails in some RTD
   Input=acum1;
   if (acum1<= Ax1)
   {
     Input = acum2;
-   
   }
   if(acum2<=Ax1)
   {
     Input = acum1;
-  
   }
-     
+
   if (Input < Ax1 or Input < Bx1)
   {
    myPID.SetMode(MANUAL);
@@ -135,11 +131,11 @@ void loop()
   {
   Input =acum1;
   }*/
-  
-  //Input = acum1;
+
+  // Input = acum1;
   myPID.Compute();
 
-//prioridad en control
+// Control priority
 #ifdef _uno
   analogWrite(9,Output);
   analogWrite(10,Output);
@@ -159,58 +155,53 @@ if (Output>0)
   {
    lcd.print(" OUT:OFF");   
   }
-//mostramos dato al final  
-      
-  
+// Data show the end
+
   menu1();
-  
-//este es para debug se puede sacar
-  
-  //valores analogicos 1 y 2
+
+// This is for debug can be removed
+
+  // Analog values ​​1 and 2
   /*
-  Serial.print("Entrada de la RTD 1--> ");
+  Serial.print("Input RTD 1--> ");
   Serial.println(t1);
-  
-  Serial.print("Entrada de la RTD 2--> ");
+
+  Serial.print("Input RTD 2--> ");
   Serial.println(t2);
   */
   
-  //temperaturas 1 y 2
+  // Temperatures 1 and 2
   /*
-  Serial.print("Temperatura t0: ");
+  Serial.print("temperature t0: ");
   Serial.println(temp1);
-  Serial.print("Temperatura t1: ");
+  Serial.print("temperature t1: ");
   Serial.println(temp2);
   */
   
   /*
-  Serial.print("Entrada de la RTD Acum 10000 PV--> ");
+  Serial.print("Input RTD Acum 10000 PV--> ");
   Serial.println(acum1);
-  Serial.print("Entrada de la RTD Acum 10000 PV--> ");
+  Serial.print("Input RTD Acum 10000 PV--> ");
   Serial.println(acum2);
   */
   
   /*
-  Serial.print("Temperatura -> ");
+  Serial.print("Temperature -> ");
   Serial.println(temp);
   */
   
-  Serial.print("Input de temp --> ");
+  Serial.print("Input temp --> ");
   Serial.println(Input);
   
     
-  Serial.print("Setpoint de temp --> ");
+  Serial.print("Setpoint temp --> ");
   Serial.println(Setpoint);
   
-  Serial.print("Salida Output");
+  Serial.print("Output");
   Serial.println(Output);
-   
-
-
 }
 
-//funcion pendiente con respecto a dos puntos se acuerdan de las matematicas 
-// y te preguntabas para que fuck servia esto jejeje
+// Slope function with respect to two points remember math and wondered served to fuck this lol
 
 float pendiente(float y1,float x1,float y2, float x2)
 {
@@ -219,7 +210,7 @@ float pendiente(float y1,float x1,float y2, float x2)
  return m;
 }
 
-//la constante la obtengo despejando 
+// I get the constant clearing
 float constante(float m,float y1,float x1)
 {
   float c=0;   
@@ -227,7 +218,7 @@ float constante(float m,float y1,float x1)
   return c;
 }
 
-//esta es la funcion lineal
+// esta es la funcion lineal
 float obt_temp(float m,float c,float x1)
 {
   float y1=(m*x1)+c;
@@ -235,8 +226,7 @@ float obt_temp(float m,float c,float x1)
 }
 
 
-
-//esta es una funcion inversa le damos la temp y nos devuelve el ADC para el PID
+// esta es una funcion inversa le damos la temp y nos devuelve el ADC para el PID
 float obt_adc(float m,float c,float y1)
 {
   float x1=(y1 - c)/m;
@@ -244,55 +234,39 @@ float obt_adc(float m,float c,float y1)
 }
 
 
-  
 void menu1()
 {
-  
-  
 analogReference(DEFAULT);
-
   int boton=analogRead(0);
   Serial.println(boton);
   if (boton == 0)
        {       
-         Serial.println("rigt");
-       //right
-       
+         Serial.println("Right");  // Right
        }else
-  
   if (boton > 90 && boton < 200)   // 90 - 200
        {
-       Serial.println("up");
+       Serial.println("Up");
        setP=setP++;
        } else 
   if (boton > 200 && boton < 300)
        {       
-       Serial.println("down");//down
+       Serial.println("Down");     // Down
        setP=setP--;
        }else
   if (boton > 400 && boton < 500)
        {       
-         Serial.println("left");
-       //left
+         Serial.println("Left");   // Left
        }else
-      //
   if (boton > 600 && boton < 700)
        {       
-         Serial.println("select");
-       //select
-       
+         Serial.println("Select"); // Select
        }
   
   #ifdef _uno    
-  analogReference(INTERNAL); //arduino mega 2560
+  analogReference(INTERNAL);       // Arduino UNO
   #endif
-       
+
   #ifdef _mega_2560    
-  analogReference(INTERNAL1V1); //arduino mega 2560
+  analogReference(INTERNAL1V1);    // Arduino Mega 2560
   #endif
-  
-  
-
 }
-
-
